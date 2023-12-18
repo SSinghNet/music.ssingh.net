645b31cadce841e652719c1f45b674500f7f22dd
Sumeet Singh <sumeet605@gmail.com>
2023-12-18 18:13:56 -0500

    year sort on charts

---
+++ public/scripts/chartYear.js
@@ -0,0 +1,20 @@
+const yearRedirect = () => {
+        let value = $("#year").val();
+        var url = new URL(window.location.href);
+        let values = value.split(" ")
+    
+        url.searchParams.set('year', values[0]);
+    
+        window.location.replace(url);
+};
+
+$(document).ready(() => {
+    var url = new URL(window.location.href);
+    let year = url.searchParams.get("year");
+    let yearSel = document.getElementById("year");
+
+    yearSel.value = year;
+    if (yearSel.value === "") {
+        yearSel.value = "all-time";
+    } 
+});
--- routes/charts.js
+++ routes/charts.js
@@ -7,9 +7,13 @@ router.get("/", async (req, res) => {
     let albums;
     let pageTitle = "Top Albums of ";
 
+    if (isNaN(req.query.year) || (req.query.year).length != 4) {
+        req.query.year = null;
+    }
+
     if (req.query.year != null) {
         albums = await Album.findAll({
-            where: sequelize.where(sequelize.fn("YEAR", sequelize.col("releaseDate")), req.query.year),
+            where: sequelize.where(sequelize.literal("CAST(SUBSTR(releaseDate, 1, 4) AS integer)"), req.query.year),
             order: [
                 ['score', 'DESC'],
             ],
