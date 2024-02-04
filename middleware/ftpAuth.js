2dabba80ea85498a5840744d61a70544c6ec8cfc
Sumeet Singh <sumeet605@gmail.com>
2024-02-04 16:02:53 -0500

    ftptest5

--- middleware/ftpAuth.js
+++ middleware/ftpAuth.js
@@ -14,7 +14,7 @@ export const uploadFileToMediaServer = async () => {
         });
 
         const file = fs.createWriteStream("file.jpg");
-        const request = https.get("https://e.snmc.io/i/600/w/3e05081dfc0a8b74a3f1301702a3f40e/11440905/tapir-the-pilgrim-their-god-and-the-king-of-my-decrepit-mountain-Cover-Art.jpg", function(response) {
+        const request = https.get("https://e.snmc.io/i/600/w/3e05081dfc0a8b74a3f1301702a3f40e/11440905/tapir-the-pilgrim-their-god-and-the-king-of-my-decrepit-mountain-Cover-Art.jpg", async (response) => {
             response.pipe(file);
 
             // after download completed close filestream
@@ -22,8 +22,9 @@ export const uploadFileToMediaServer = async () => {
                 file.close();
                 console.log("Download Completed");
             });
+            await client.uploadFrom("file.jpg", "media.ssingh.net/music/albums/test4.jpg");
+
         });
-        await client.uploadFrom("file.jpg", "media.ssingh.net/music/albums/test4.jpg");
 
         fs.unlink("file.jpg", () => { });
 
