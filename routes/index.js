fd879b79b522fe73956e0462d2a52e9e627600cc
Sumeet Singh <sumeet605@gmail.com>
2024-02-09 21:15:31 -0500

    add pages to home

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,34 @@ import { Album, Artist, Tag } from "../models/models.js";
 export let router = express.Router();
 
 router.get("/", async (req, res) => {
-    let albs = await Album.findAll({ include:[Artist, Tag], order: [
-        ['updatedAt', 'DESC'],
-    ]});
+    let page = 0
+    let amount = 21
 
-    res.render("index", { title: "Home - ", albums: albs });
+    const count = await Album.count();
+    console.log(count);
+
+    let pageIn = req.query.page;
+
+    try {
+        if (pageIn != null) {
+            if (typeof parseInt(pageIn) === "number" && (count / amount) > (pageIn)) {
+                page = parseInt(pageIn);
+            }
+        }
+    } catch {
+        console.log("L");
+    }
+
+    // page = req.query.page;
+
+    let albs = await Album.findAll({
+        include: [Artist, Tag],
+        order: [
+            ['updatedAt', 'DESC'],
+        ],
+        limit: amount + 1,
+        offset: page != 0 ? amount * page + 1: 0
+    });
+
+    res.render("index", { title: "Home - ", albums: albs, page: page, maxPage: count / amount });
 });
